1ca1d59f26152fed5e602d5e4b101ec87d8152c9
Paul Schmitz <p_schmitz@email.com>
2024-04-02 21:29:24 -0500

    add tiles and player controller
    completed tutorials 1 thru 4

---
+++ My project (2)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public LayerMask solidObjectsLayer;
+    public LayerMask grassLayer;
+    public bool isMoving;
+    private Vector2 input;
+
+    private Animator animator;
+    private void Awake(){
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        if (!isMoving){
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+            if (input != Vector2.zero){
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                if (IsWalkable(targetPos)) {
+                    StartCoroutine(Move(targetPos));
+                }
+                
+            }
+        }
+
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    IEnumerator Move(Vector3 targetPos){
+        isMoving = true;
+        while((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+
+        CheckForEncounters();
+    }
+
+    private bool IsWalkable(Vector3 targetPos){
+       return Physics2D.OverlapCircle(targetPos, 0.1f, solidObjectsLayer) == null;
+    }
+
+    private void CheckForEncounters(){
+        if (Physics2D.OverlapCircle(transform.position, 0.1f, grassLayer) != null){
+            // player walked on grass
+            if (Random.Range(1, 101) <= 10){
+                Debug.Log("Encountered a wild pokemon");
+            }
+        }
+    }
+}
